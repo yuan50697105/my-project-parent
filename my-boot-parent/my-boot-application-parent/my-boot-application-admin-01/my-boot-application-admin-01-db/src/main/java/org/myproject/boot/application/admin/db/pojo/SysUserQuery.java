9dc5720a0593f2966311dcc946c85d0bac1c746f
yuan50697105 <33301332+yuan50697105@users.noreply.github.com>
2020-06-25 19:42:43 +0800

    webmvc修改名称

--- my-boot-parent/my-boot-application-parent/my-boot-application-admin-01/my-boot-application-admin-01-db/src/main/java/org/myproject/boot/application/admin/db/pojo/SysUserQuery.java
+++ my-boot-parent/my-boot-application-parent/my-boot-application-admin-01/my-boot-application-admin-01-db/src/main/java/org/myproject/boot/application/admin/db/pojo/SysUserQuery.java
@@ -3,9 +3,7 @@ package org.myproject.boot.application.admin.db.pojo;
 import com.gitee.denger.mybatis.example.ext.MybatisExample;
 import com.gitee.denger.mybatis.example.ext.annotation.AndEqualTo;
 import com.gitee.denger.mybatis.example.ext.annotation.AndLike;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.myproject.mybatis.pagehelper.pojo.BaseEntity;
 
 import java.io.Serializable;
@@ -16,6 +14,8 @@ import java.io.Serializable;
 @Data
 @EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class SysUserQuery extends BaseEntity implements Serializable, MybatisExample<SysUserExample> {
     private static final long serialVersionUID = 1L;
     /**
