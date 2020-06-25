bd88136be9bc157d175e1009e9a582fec945c272
yuan50697105 <33301332+yuan50697105@users.noreply.github.com>
2020-06-25 14:17:52 +0800

    sss

---
+++ my-boot-parent/my-boot-application-parent/my-boot-application-admin-01/my-boot-application-admin-db/src/main/java/org/myproject/application/admin/db/pojo/EnabledState.java
@@ -0,0 +1,14 @@
+package org.myproject.application.admin.db.pojo;
+
+public enum EnabledState {
+    ENABLED("ENABLED"), DISABLE("DISABLE");
+    private final String state;
+
+    EnabledState(String state) {
+        this.state = state;
+    }
+
+    public String getState() {
+        return state;
+    }
+}
