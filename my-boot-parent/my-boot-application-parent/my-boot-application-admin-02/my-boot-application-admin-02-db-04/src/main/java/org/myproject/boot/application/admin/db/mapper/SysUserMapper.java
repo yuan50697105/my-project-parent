c1fe9dc7bcabaf68cc7361c45edfa98e3f4fee91
yuan50697105 <33301332+yuan50697105@users.noreply.github.com>
2020-06-27 21:34:16 +0800

    my-boot-application-admin-02

---
+++ my-boot-parent/my-boot-application-parent/my-boot-application-admin-02/my-boot-application-admin-02-db-04/src/main/java/org/myproject/boot/application/admin/db/mapper/SysUserMapper.java
@@ -0,0 +1,22 @@
+package org.myproject.boot.application.admin.db.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.myproject.boot.application.admin.db.pojo.SysUser;
+import org.myproject.boot.application.admin.db.pojo.SysUserExample;
+
+import java.util.List;
+
+@Mapper
+public interface SysUserMapper extends BaseMapper<SysUser> {
+    long countByExample(SysUserExample example);
+
+    int deleteByExample(SysUserExample example);
+
+    List<SysUser> selectByExample(SysUserExample example);
+
+    int updateByExampleSelective(@Param("record") SysUser record, @Param("example") SysUserExample example);
+
+    int updateByExample(@Param("record") SysUser record, @Param("example") SysUserExample example);
+}
