101cd883a8b317768d8fd5c688693f76ea185988
yuan50697105 <33301332+yuan50697105@users.noreply.github.com>
2020-06-27 21:46:12 +0800

    my-boot-application-admin-02

---
+++ my-boot-parent/my-boot-application-parent/my-boot-application-admin-02/my-boot-application-admin-02-cxf-04/src/main/java/org/myproject/boot/application/admin/cxf/webservice/WebUserRsService.java
@@ -0,0 +1,28 @@
+package org.myproject.boot.application.admin.cxf.webservice;
+
+import ai.yue.library.base.view.Result;
+import org.myproject.boot.application.admin.db.pojo.SysUser;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
+
+/**
+ * @program: my-project-parent
+ * @description:
+ * @author: yuane
+ * @create: 2020-06-25 18:28
+ */
+@Path("web/user/rs/service")
+public interface WebUserRsService {
+    String SERVICE_NAME = "WebUserRsService";
+
+    @GET
+    @Path("aa")
+    String aa();
+
+    @POST
+    @Path("save")
+    Result<?> save(@Context SysUser sysUser);
+}
