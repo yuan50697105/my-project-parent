3d3eab553114d4ad6e0782a333e4644fbb9655b5
yuan50697105 <33301332+yuan50697105@users.noreply.github.com>
2020-06-27 20:49:24 +0800

    my-boot-application-admin-02

---
+++ my-boot-parent/my-boot-application-parent/my-boot-application-admin-01/my-boot-application-admin-01-jersey-08/src/main/java/org/myproject/boot/application/admin/jersey/service/UserService.java
@@ -0,0 +1,11 @@
+package org.myproject.boot.application.admin.jersey.service;
+
+/**
+ * @program: my-project-parent
+ * @description:
+ * @author: yuane
+ * @create: 2020-06-25 19:10
+ */
+public interface UserService {
+    String hello();
+}
