8891501ac9625bfddc9cd3b9b6c3a657dfe6622b
yuan50697105 <33301332+yuan50697105@users.noreply.github.com>
2020-06-27 13:04:30 +0800

    设计Spring Security

---
+++ my-boot-parent/my-boot-application-parent/my-boot-application-admin-01/my-boot-application-admin-01-webmvc-04/src/main/java/org/myproject/boot/application/admin/mvc/Mvc01Configuration.java
@@ -0,0 +1,13 @@
+package org.myproject.boot.application.admin.mvc;
+
+import org.springframework.boot.SpringBootConfiguration;
+
+/**
+ * @program: my-project-parent
+ * @description:
+ * @author: yuane
+ * @create: 2020-06-27 12:57
+ */
+@SpringBootConfiguration
+public class Mvc01Configuration {
+}
