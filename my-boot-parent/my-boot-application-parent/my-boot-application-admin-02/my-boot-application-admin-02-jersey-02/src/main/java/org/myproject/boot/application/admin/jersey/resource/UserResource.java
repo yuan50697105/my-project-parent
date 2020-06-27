d288df0171f2ca08f091a6d301b631905d807b41
yuan50697105 <33301332+yuan50697105@users.noreply.github.com>
2020-06-27 20:53:37 +0800

    my-boot-application-admin-02

---
+++ my-boot-parent/my-boot-application-parent/my-boot-application-admin-02/my-boot-application-admin-02-jersey-02/src/main/java/org/myproject/boot/application/admin/jersey/resource/UserResource.java
@@ -0,0 +1,28 @@
+package org.myproject.boot.application.admin.jersey.resource;
+
+import org.myproject.boot.application.admin.jersey.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+/**
+ * @program: my-project-parent
+ * @description:
+ * @author: yuane
+ * @create: 2020-06-25 19:10
+ */
+@Component
+@Path("user")
+public class UserResource {
+    @Autowired
+    private UserService userService;
+
+    @GET
+    @Path("hello")
+    public String hello() {
+        return userService.hello();
+    }
+
+}
