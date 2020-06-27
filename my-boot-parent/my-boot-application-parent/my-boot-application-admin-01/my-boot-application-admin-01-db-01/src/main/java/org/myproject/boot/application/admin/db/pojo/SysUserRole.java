e51747ade4b235ad31aa0dfc4ec807e61d449e9c
yuan50697105 <33301332+yuan50697105@users.noreply.github.com>
2020-06-27 14:35:47 +0800

    设计Spring Security

---
+++ my-boot-parent/my-boot-application-parent/my-boot-application-admin-01/my-boot-application-admin-01-db-01/src/main/java/org/myproject/boot/application/admin/db/pojo/SysUserRole.java
@@ -0,0 +1,22 @@
+package org.myproject.boot.application.admin.db.pojo;
+
+import lombok.*;
+import org.myproject.boot.mybatis.pagehelper.pojo.BaseEntity;
+
+import java.io.Serializable;
+
+/**
+ * 用户角色
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class SysUserRole extends BaseEntity implements Serializable {
+    private Long userId;
+
+    private Long roleId;
+
+    private static final long serialVersionUID = 1L;
+}
