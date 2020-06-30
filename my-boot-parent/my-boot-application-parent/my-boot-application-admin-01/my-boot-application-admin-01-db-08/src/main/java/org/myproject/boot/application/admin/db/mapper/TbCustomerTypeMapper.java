ebe9800cf8710a8ef89ac797cf9097d42000c07f
yuan50697105 <33301332+yuan50697105@users.noreply.github.com>
2020-06-30 19:17:48 +0800

    routeList

--- my-boot-parent/my-boot-application-parent/my-boot-application-admin-01/my-boot-application-admin-01-db-08/src/main/java/org/myproject/boot/application/admin/db/mapper/TbCustomerTypeMapper.java
+++ my-boot-parent/my-boot-application-parent/my-boot-application-admin-01/my-boot-application-admin-01-db-08/src/main/java/org/myproject/boot/application/admin/db/mapper/TbCustomerTypeMapper.java
@@ -1,14 +1,15 @@
 package org.myproject.boot.application.admin.db.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.myproject.boot.application.admin.db.pojo.TbCustomerType;
 import org.myproject.boot.application.admin.db.pojo.TbCustomerTypeExample;
 
+import java.util.List;
+
 @Mapper
-public interface TbCustomerTypeMapper extends BaseMapper<TbCustomerType> {
+public interface TbCustomerTypeMapper extends BaseMapper<TbCustomerType>, tk.mybatis.mapper.common.Mapper<TbCustomerType> {
     long countByExample(TbCustomerTypeExample example);
 
     int deleteByExample(TbCustomerTypeExample example);
---
+++ my-boot-parent/my-boot-application-parent/my-boot-application-admin-01/my-boot-application-admin-01-db-08/src/main/java/org/myproject/boot/application/admin/db/service/impl/SysUserRouteServiceImpl.java
@@ -0,0 +1,25 @@
+package org.myproject.boot.application.admin.db.service.impl;
+
+import org.myproject.boot.application.admin.db.mapper.SysUserRouteMapper;
+import org.myproject.boot.application.admin.db.service.SysUserRouteService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @program: my-boot-parent
+ * @description:
+ * @author: yuane
+ * @create: 2020-06-30 19:16
+ */
+@Service
+public class SysUserRouteServiceImpl implements SysUserRouteService {
+    @Resource
+    private SysUserRouteMapper sysUserRouteMapper;
+
+    @Override
+    public List<String> selectByUserId(Long userId) {
+        return sysUserRouteMapper.selectRouteUrlByUserId(userId);
+    }
+}
