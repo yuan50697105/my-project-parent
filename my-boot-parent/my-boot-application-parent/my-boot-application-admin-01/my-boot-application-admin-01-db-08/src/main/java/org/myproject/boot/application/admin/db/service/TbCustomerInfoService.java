e4df83ca077e64ce577d9abf483182b7c27b95f1
yuan50697105 <33301332+yuan50697105@users.noreply.github.com>
2020-06-30 19:49:54 +0800

    routeList

--- my-boot-parent/my-boot-application-parent/my-boot-application-admin-01/my-boot-application-admin-01-db-08/src/main/java/org/myproject/boot/application/admin/db/service/TbCustomerInfoService.java
+++ my-boot-parent/my-boot-application-parent/my-boot-application-admin-01/my-boot-application-admin-01-db-08/src/main/java/org/myproject/boot/application/admin/db/service/TbCustomerInfoService.java
@@ -1,9 +1,13 @@
 package org.myproject.boot.application.admin.db.service;
 
-import org.myproject.boot.application.admin.db.pojo.TbCustomerInfoExample;
-import java.util.List;
-import org.myproject.boot.application.admin.db.pojo.TbCustomerInfo;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.github.pagehelper.PageInfo;
+import org.myproject.boot.application.admin.db.pojo.TbCustomerInfo;
+import org.myproject.boot.application.admin.db.pojo.TbCustomerInfoExample;
+import org.myproject.boot.application.admin.db.pojo.TbCustomerInfoQuery;
+
+import java.util.List;
+
 public interface TbCustomerInfoService extends IService<TbCustomerInfo>{
 
 
@@ -17,4 +21,7 @@ public interface TbCustomerInfoService extends IService<TbCustomerInfo>{
 
     int updateByExample(TbCustomerInfo record,TbCustomerInfoExample example);
 
+    PageInfo<TbCustomerInfo> selectByQueryWithPage(int page, int size, TbCustomerInfoQuery query);
+
+    List<TbCustomerInfo> selectByQuery(TbCustomerInfoQuery query);
 }
