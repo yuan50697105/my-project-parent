7c56c8572ee2073fc9e8f9b4ff5f2fa7dca8a720
yuan50697105 <33301332+yuan50697105@users.noreply.github.com>
2020-06-30 19:32:49 +0800

    routeList

--- my-boot-parent/my-boot-application-parent/my-boot-application-admin-01/my-boot-application-admin-01-db-07/src/main/java/org/myproject/boot/application/admin/db/service/TbCustomerInfoService.java
+++ my-boot-parent/my-boot-application-parent/my-boot-application-admin-01/my-boot-application-admin-01-db-07/src/main/java/org/myproject/boot/application/admin/db/service/TbCustomerInfoService.java
@@ -1,9 +1,12 @@
 package org.myproject.boot.application.admin.db.service;
 
+import com.github.pagehelper.PageInfo;
 import org.myproject.boot.application.admin.db.pojo.TbCustomerInfoExample;
 import java.util.List;
 import org.myproject.boot.application.admin.db.pojo.TbCustomerInfo;
 import com.baomidou.mybatisplus.extension.service.IService;
+import org.myproject.boot.application.admin.db.pojo.TbCustomerInfoQuery;
+
 public interface TbCustomerInfoService extends IService<TbCustomerInfo>{
 
 
@@ -16,5 +19,9 @@ public interface TbCustomerInfoService extends IService<TbCustomerInfo>{
     int updateByExampleSelective(TbCustomerInfo record,TbCustomerInfoExample example);
 
     int updateByExample(TbCustomerInfo record,TbCustomerInfoExample example);
+
+    PageInfo<TbCustomerInfo> selectByQueryWithPage(int page, int size, TbCustomerInfoQuery query);
+
+    List<TbCustomerInfo> selectByQuery(TbCustomerInfoQuery query);
 
 }
--- my-boot-parent/my-boot-application-parent/my-boot-application-admin-01/my-boot-application-admin-01-db-07/src/main/java/org/myproject/boot/application/admin/db/service/impl/TbCustomerInfoServiceImpl.java
+++ my-boot-parent/my-boot-application-parent/my-boot-application-admin-01/my-boot-application-admin-01-db-07/src/main/java/org/myproject/boot/application/admin/db/service/impl/TbCustomerInfoServiceImpl.java
@@ -1,14 +1,16 @@
 package org.myproject.boot.application.admin.db.service.impl;
 
-import org.springframework.stereotype.Service;
-import javax.annotation.Resource;
-import java.util.List;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import org.myproject.boot.application.admin.db.pojo.TbCustomerInfoExample;
-import java.util.List;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.myproject.boot.application.admin.db.mapper.TbCustomerInfoMapper;
 import org.myproject.boot.application.admin.db.pojo.TbCustomerInfo;
+import org.myproject.boot.application.admin.db.pojo.TbCustomerInfoExample;
+import org.myproject.boot.application.admin.db.pojo.TbCustomerInfoQuery;
 import org.myproject.boot.application.admin.db.service.TbCustomerInfoService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
 @Service
 public class TbCustomerInfoServiceImpl extends ServiceImpl<TbCustomerInfoMapper, TbCustomerInfo> implements TbCustomerInfoService{
 
@@ -31,5 +33,16 @@ public class TbCustomerInfoServiceImpl extends ServiceImpl<TbCustomerInfoMapper,
     @Override
     public int updateByExample(TbCustomerInfo record,TbCustomerInfoExample example) {
         return baseMapper.updateByExample(record,example);
+    }
+
+    @Override
+    public List<TbCustomerInfo> selectByQuery(TbCustomerInfoQuery query) {
+        return baseMapper.selectByExample(query.toExample());
+    }
+
+    @Override
+    public PageInfo<TbCustomerInfo> selectByQueryWithPage(int page, int size, TbCustomerInfoQuery query) {
+        PageHelper.startPage(page, size);
+        return new PageInfo<>(baseMapper.selectByExample(query.toExample()));
     }
 }
