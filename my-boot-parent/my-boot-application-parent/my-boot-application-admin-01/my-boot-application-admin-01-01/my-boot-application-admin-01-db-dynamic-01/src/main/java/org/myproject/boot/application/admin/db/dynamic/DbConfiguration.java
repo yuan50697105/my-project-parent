88cbea4cf387d8d4f3104b659fa7667504af8c70
yuan50697105 <33301332+yuan50697105@users.noreply.github.com>
2020-07-07 14:53:52 +0800

    ...

---
+++ my-boot-parent/my-boot-application-parent/my-boot-application-admin-01/my-boot-application-admin-01-01/my-boot-application-admin-01-db-dynamic-01/src/main/java/org/myproject/boot/application/admin/db/dynamic/DbConfiguration.java
@@ -0,0 +1,15 @@
+package org.myproject.boot.application.admin.db.dynamic;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @program: my-project-parent
+ * @description:
+ * @author: yuane
+ * @create: 2020-06-25 19:47
+ */
+@SpringBootConfiguration
+@ComponentScan(basePackageClasses = DbConfiguration.class)
+public class DbConfiguration {
+}
