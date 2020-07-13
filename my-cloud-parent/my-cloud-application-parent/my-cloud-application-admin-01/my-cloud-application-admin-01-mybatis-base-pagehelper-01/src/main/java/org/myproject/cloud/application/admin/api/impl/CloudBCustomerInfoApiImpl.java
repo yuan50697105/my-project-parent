369c21c2648e4511d155f5c35802584c2477852c
yuan50697105 <33301332+yuan50697105@users.noreply.github.com>
2020-07-13 08:56:21 +0800

    删除错误文件

--- my-cloud-parent/my-cloud-application-parent/my-cloud-application-admin-01/my-cloud-application-admin-01-mybatis-base-pagehelper-01/src/main/java/org/myproject/cloud/application/admin/api/impl/CloudBCustomerInfoApiImpl.java
+++ my-cloud-parent/my-cloud-application-parent/my-cloud-application-admin-01/my-cloud-application-admin-01-mybatis-base-pagehelper-01/src/main/java/org/myproject/cloud/application/admin/api/impl/CloudBCustomerInfoApiImpl.java
@@ -8,6 +8,9 @@ import org.myproject.boot.application.admin.service.pojo.CustomerInfoQuery;
 import org.myproject.boot.application.admin.service.pojo.CustomerInfoVo;
 import org.myproject.boot.mybatis.commons.pojo.IPage;
 import org.myproject.cloud.application.admin.api.CloudBCustomerInfoApi;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,6 +22,7 @@ import java.util.List;
  * @create: 2020-07-13 00:06
  */
 @RestController
+@RequestMapping("customer/info")
 public class CloudBCustomerInfoApiImpl extends CustomerInfoApiImpl implements CloudBCustomerInfoApi {
     private final TbConverter converter;
     private final TbCustomerInfoService customerInfoService;
@@ -30,26 +34,31 @@ public class CloudBCustomerInfoApiImpl extends CustomerInfoApiImpl implements Cl
     }
 
     @Override
+    @RequestMapping(value = "page", method = {RequestMethod.GET})
     public IPage<CustomerInfo> selectByQuery(CustomerInfoQuery query, int page, int size) {
         return super.selectByQuery(query, page, size);
     }
 
     @Override
+    @RequestMapping(value = "list", method = {RequestMethod.GET})
     public List<CustomerInfo> selectByQuery(CustomerInfoQuery query) {
         return super.selectByQuery(query);
     }
 
     @Override
-    public CustomerInfo get(Long id) {
+    @RequestMapping(value = {"{id}"}, method = {RequestMethod.GET})
+    public CustomerInfo get(@PathVariable Long id) {
         return super.get(id);
     }
 
     @Override
+    @RequestMapping(method = {RequestMethod.POST})
     public void insert(CustomerInfoVo vo) {
         super.insert(vo);
     }
 
     @Override
+    @RequestMapping(method = {RequestMethod.PUT})
     public void update(CustomerInfoVo vo) {
         super.update(vo);
     }
@@ -60,7 +69,8 @@ public class CloudBCustomerInfoApiImpl extends CustomerInfoApiImpl implements Cl
     }
 
     @Override
-    public void delete(Long id) {
+    @RequestMapping(value = "{id}", method = {RequestMethod.DELETE})
+    public void delete(@PathVariable Long id) {
         super.delete(id);
     }
 }
