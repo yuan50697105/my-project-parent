2e326761dcd40743acbfd4b3d9b59209a4fa44a3
yuan50697105 <33301332+yuan50697105@users.noreply.github.com>
2020-07-13 12:33:07 +0800

    删除错误文件

--- my-boot-parent/my-boot-application-parent/my-boot-application-admin-01/my-boot-application-admin-01-01/my-boot-application-admin-01-webmvc-01/src/main/java/org/myproject/boot/application/admin/mvc/Mvc01Configuration.java
+++ my-boot-parent/my-boot-application-parent/my-boot-application-admin-01/my-boot-application-admin-01-01/my-boot-application-admin-01-webmvc-01/src/main/java/org/myproject/boot/application/admin/mvc/Mvc01Configuration.java
@@ -1,7 +1,12 @@
 package org.myproject.boot.application.admin.mvc;
 
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
+import org.minbox.framework.api.boot.autoconfigure.swagger.annotation.EnableApiBootSwagger;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @program: my-project-parent
@@ -10,6 +15,10 @@ import org.springframework.context.annotation.ComponentScan;
  * @create: 2020-06-27 12:57
  */
 @SpringBootConfiguration
+@EnableApiBootSwagger
+@EnableSwagger2
+@EnableKnife4j
+@Import(BeanValidatorPluginsConfiguration.class)
 @ComponentScan(basePackageClasses = Mvc01Configuration.class)
 public class Mvc01Configuration {
 }
