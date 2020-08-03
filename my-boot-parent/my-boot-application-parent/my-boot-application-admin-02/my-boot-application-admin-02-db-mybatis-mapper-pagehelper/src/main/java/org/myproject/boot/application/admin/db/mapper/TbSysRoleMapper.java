1534be3a9ad1d9336f22f71ced1d5bf676c7f15d
yuan50697105 <710575900@qq.com>
2020-08-03 23:55:31 +0800

    升级依赖

--- my-boot-parent/my-boot-application-parent/my-boot-application-admin-02/my-boot-application-admin-02-db-mybatis-mapper-pagehelper/src/main/java/org/myproject/boot/application/admin/db/mapper/TbSysRoleMapper.java
+++
@@ -1,8 +0,0 @@
-package org.myproject.boot.application.admin.db.mapper;
-
-import org.apache.ibatis.annotations.Mapper;
-import org.myproject.boot.application.admin.db.pojo.TbSysRole;
-
-@Mapper
-public interface TbSysRoleMapper extends tk.mybatis.mapper.common.Mapper<TbSysRole> {
-}
