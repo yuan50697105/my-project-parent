6ed4a59c59153635efb5a99c3e17f770a37a7b67
yuan50697105 <710575900@qq.com>
2020-08-04 14:41:05 +0800

    zzz

---
+++ my-boot-parent/my-boot-application-parent/my-boot-application-admin-01/my-boot-application-admin-01-db-mybatis-plus-pagehelper-01/src/main/java/org/myproject/boot/application/admin/api/pojo/PageResult.java
@@ -0,0 +1,19 @@
+package org.myproject.boot.application.admin.api.pojo;
+
+import com.github.pagehelper.PageInfo;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * @program: my-project-parent
+ * @description:
+ * @author: yuane
+ * @create: 2020-08-04 14:39
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class PageResult<T> extends IPage<T> {
+    public PageResult(PageInfo<T> pageInfo) {
+        super(pageInfo.getPageNum(), pageInfo.getSize(), pageInfo.getTotal(), pageInfo.getPages(), pageInfo.getList());
+    }
+}
