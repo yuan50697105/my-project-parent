454a2ad5ea6b358c31da7358c9e8c3676305254d
yuan50697105 <710575900@qq.com>
2020-08-09 10:35:08 +0800

    admin-02

---
+++ my-boot-parent/my-boot-application-parent/my-boot-application-admin-02/my-boot-application-admin-02-restful-api-12/src/main/java/org/myproject/boot/application/admin/api/pojo/SysUsersQuery.java
@@ -0,0 +1,17 @@
+package org.myproject.boot.application.admin.api.pojo;
+
+import lombok.Data;
+
+/**
+ * @program: my-boot-application-admin-02
+ * @description:
+ * @author: yuane
+ * @create: 2020-08-07 22:25
+ */
+@Data
+public class SysUsersQuery {
+    private Long id;
+    private String username;
+    private String name;
+
+}
