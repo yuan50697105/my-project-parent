c99abb7517f5d27661e911e9015192d182b5455e
yuan50697105 <710575900@qq.com>
2020-08-11 02:01:14 +0800

    refactor:(goods):sql脚本

---
+++ my-boot-parent/my-boot-application-parent/my-boot-application-admin-02/my-boot-application-admin-02-modules/my-boot-application-admin-02-modules-goods/my-boot-application-admin-02-modules-goods-db-01/src/main/java/org/myproject/boot/application/goods/service/ServiceConfiguration.java
@@ -0,0 +1,17 @@
+package org.myproject.boot.application.goods.service;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @program: my-project-parent
+ * @description:
+ * @author: yuane
+ * @create: 2020-08-04 01:03
+ */
+@SpringBootConfiguration
+@ComponentScan(basePackages = "org.myproject.boot.application.admin.db")
+@MapperScan(basePackages = "org.myproject.boot.application.admin.db.mapper")
+public class ServiceConfiguration {
+}
