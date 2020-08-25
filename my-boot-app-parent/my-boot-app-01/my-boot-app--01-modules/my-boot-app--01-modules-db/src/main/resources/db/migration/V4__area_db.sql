8c5e31a851013830f3444777803de9ef6c76738e
yuan50697105 <710575900@qq.com>
2020-08-25 09:56:09 +0800

    i

--- my-boot-app-parent/my-boot-app-01/my-boot-app--01-modules/my-boot-app--01-modules-db/src/main/resources/db/migration/V4__area_db.sql
+++ my-boot-app-parent/my-boot-app-01/my-boot-app--01-modules/my-boot-app--01-modules-db/src/main/resources/db/migration/V4__area_db.sql
@@ -9,3 +9,13 @@ CREATE TABLE if not exists area_info
     PRIMARY KEY (id)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4 COMMENT 'area_info';
+-- auto Generated on 2020-08-23
+-- DROP TABLE IF EXISTS area_address;
+CREATE TABLE if not exists area_address
+(
+    id      BIGINT(15)  NOT NULL AUTO_INCREMENT COMMENT 'id',
+    area_id BIGINT(15)  NOT NULL DEFAULT -1 COMMENT 'areaId',
+    address VARCHAR(50) NOT NULL DEFAULT '' COMMENT 'address',
+    PRIMARY KEY (id)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4 COMMENT 'area_address';
