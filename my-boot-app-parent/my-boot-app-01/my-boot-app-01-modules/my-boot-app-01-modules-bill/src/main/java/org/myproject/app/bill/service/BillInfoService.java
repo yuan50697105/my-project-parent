bdef83c242e5c8705c53973ac19857c50d826cbe
yuan50697105 <710575900@qq.com>
2020-08-28 17:14:38 +0800

    [新增功能](billInfo): 新增订单信息
    新增订单信息

---
+++ my-boot-app-parent/my-boot-app-01/my-boot-app-01-modules/my-boot-app-01-modules-bill/src/main/java/org/myproject/app/bill/service/BillInfoService.java
@@ -0,0 +1,47 @@
+package org.myproject.app.bill.service;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.myproject.app.bill.pojo.BillInfoExample;
+import org.myproject.app.bill.pojo.BillInfo;
+import org.myproject.app.bill.pojo.BillInfoQuery;
+import org.myproject.app.bill.pojo.BillInfoVo;
+import org.myproject.app.commons.pojo.IPage;
+
+public interface BillInfoService {
+
+
+    long countByExample(BillInfoExample example);
+
+    int deleteByExample(BillInfoExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(BillInfo record);
+
+    int insertSelective(BillInfo record);
+
+    List<BillInfo> selectByExample(BillInfoExample example);
+
+    BillInfo selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(BillInfo record, BillInfoExample example);
+
+    int updateByExample(BillInfo record, BillInfoExample example);
+
+    int updateByPrimaryKeySelective(BillInfo record);
+
+    int updateByPrimaryKey(BillInfo record);
+
+    BillInfo selectAllByIdOrBillNoOrTransactionNo(Long id, String billNo, String transactionNo);
+
+    int deleteByIdIn(Collection<Long> idCollection);
+
+    IPage<BillInfo> selectPageByQuery(BillInfoQuery query);
+
+    int insert(BillInfoVo vo);
+
+    int updateByPrimaryKeySelective(BillInfoVo vo);
+}
+
