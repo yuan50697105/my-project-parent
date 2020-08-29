322b298f83e87f3efaa6fb92cff0e935803f377b
yuan50697105 <710575900@qq.com>
2020-08-29 17:49:03 +0800

    [新增功能](my-boot-app-01-modules-customer): 完善文档
    完善文档

--- my-boot-app-parent/my-boot-app-01/my-boot-app-01-modules/my-boot-app-01-modules-customer/src/main/java/org/myproject/app/customer/controller/CustomerInfoController.java
+++ my-boot-app-parent/my-boot-app-01/my-boot-app-01-modules/my-boot-app-01-modules-customer/src/main/java/org/myproject/app/customer/controller/CustomerInfoController.java
@@ -10,8 +10,8 @@ import org.myproject.app.customer.pojo.CustomerInfo;
 import org.myproject.app.customer.pojo.CustomerInfoQuery;
 import org.myproject.app.customer.pojo.CustomerInfoVo;
 import org.myproject.app.customer.service.CustomerInfoService;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import wiki.xsx.core.log.Log;
 
 import java.util.Arrays;
 import java.util.List;
@@ -22,43 +22,53 @@ import java.util.List;
  * @author: yuane
  * @create: 2020-08-23 14:47
  */
-@Api(value = "客户信息", produces = MediaType.APPLICATION_JSON_VALUE)
+@Api(value = "客户信息")
 @RestController
 @RequestMapping("customer/info")
 @AllArgsConstructor
 public class CustomerInfoController {
+    public static final String API_QUERY = "查询客户";
+    public static final String API_ID_GET = "ID获取客户信息";
+    public static final String API_INSERT = "增加客户";
+    public static final String API_UPDATE = "更新客户";
+    public static final String API_REMOVE = "删除客户";
     private final CustomerInfoService service;
 
     @GetMapping
-    @ApiOperation("查询")
+    @ApiOperation(API_QUERY)
+    @Log(API_QUERY)
     public Result<List<CustomerInfo>> list(CustomerInfoQuery query) {
         IPage<CustomerInfo> pageInfo = service.selectPageByQuery(query);
         return ResultInfo.success(pageInfo.getTotal(), pageInfo.getList());
     }
 
     @GetMapping("/{id}")
-    @ApiOperation("ID获取")
+    @ApiOperation(API_ID_GET)
+    @Log(API_ID_GET)
     public Result<CustomerInfo> get(@PathVariable Long id) {
         CustomerInfo CustomerInfo = service.selectByPrimaryKey(id);
         return ResultInfo.success(CustomerInfo);
     }
 
     @PostMapping
-    @ApiOperation("增加")
+    @ApiOperation(API_INSERT)
+    @Log(API_INSERT)
     public Result<?> insert(@RequestBody CustomerInfoVo customerInfoVo) {
         service.insert(customerInfoVo);
         return ResultInfo.success();
     }
 
     @PutMapping
-    @ApiOperation("更新")
+    @ApiOperation(API_UPDATE)
+    @Log(API_UPDATE)
     public Result<?> update(CustomerInfoVo customerInfoVo) {
         service.updateByPrimaryKeySelective(customerInfoVo);
         return ResultInfo.success();
     }
 
     @DeleteMapping("/{id}")
-    @ApiOperation("删除")
+    @ApiOperation(API_REMOVE)
+    @Log(API_REMOVE)
     public Result<?> delete(@PathVariable Long[] id) {
         service.deleteByIdIn(Arrays.asList(id));
         return ResultInfo.success();
